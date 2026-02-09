562810ca05141b2fa7be80f1ec4e8778f3a66f43
Hanzala Chaudhary <hanzala.hc@gmail.com>
2026-02-09 14:26:06 +0500

    First Commit

---
+++ 01-FizzBuzz.js
@@ -0,0 +1,18 @@
+// Constraints: 1 <= n <= 104
+var fizzBuzz = function (n) {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    var curr = i + 1;
+    if (curr % 3 === 0 && curr % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (curr % 3 === 0) {
+      arr.push("Fizz");
+    } else if (curr % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      const strNum = curr.toString();
+      arr.push(strNum);
+    }
+  }
+  return arr;
+};
