4cff59145fd83bc1d91840de4a569871adc9b759
Hanzala Chaudhary <hanzala.hc@gmail.com>
2026-02-12 14:09:52 +0500

    optimized solution for 02

--- 02-PalindromeNumber.js
+++ 02-PalindromeNumber.js
@@ -15,6 +15,26 @@ var isPalindrome = function (x) {
   }
 };
 
-console.log(isPalindrome(101));
+// console.log(isPalindrome(101));
 
 // Follow up: Could you solve it without converting the integer to a string?
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+  if (x < 10) {
+    return true;
+  }
+  let rev = 0;
+  let left = x;
+
+  while (left > 0) {
+    let last = left % 10;
+    left = Math.floor(left / 10);
+    rev = rev * 10 + last;
+    console.log(last, left, rev);
+  }
+  return x === rev;
+};
+
+console.log(isPalindrome(101));
