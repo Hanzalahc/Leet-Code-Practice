aedf0b8d25733e2ad3b872405d2fe705661c0991
Hanzala Chaudhary <hanzala.hc@gmail.com>
2026-02-23 20:01:03 +0500

    Used hash map for two sum

---
+++ 06-TwoSum.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [i, map.get(complement)];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
